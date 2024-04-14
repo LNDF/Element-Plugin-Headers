84f46854718cf37ebb228221d253a7dc22cb754b
Lander Gallastegi <landergallastegi@gmail.com>
2024-04-15 00:48:28 +0200

    Event labels

---
+++ include/element/events.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "defs.h"
+
+ELM_BEGIN_C
+
+#define ELM_EVENT_CORE_CLOSE "element.core.close"
+
+#define ELM_EVENT_DISPLAY_MOUSE_DOWN "element.display.mouse_down"
+#define ELM_EVENT_DISPLAY_MOUSE_UP "element.display.mouse_up"
+#define ELM_EVENT_DISPLAY_KEY_DOWN "element.display.key_down"
+#define ELM_EVENT_DISPLAY_KEY_UP "element.display.key_up"
+#define ELM_EVENT_DISPLAY_MOUSE_MOVE "element.display.mouse_move"
+#define ELM_EVENT_DISPLAY_WINDOW_RESIZE "element.display.window_resize"
+
+ELM_END_C
