2b5d974a4ff67237fa58f3d7a349ac6f46baa1e5
Lander Gallastegi <landergallastegi@gmail.com>
2024-04-17 18:31:00 +0200

    Module module struct

--- include/element/modules/module.h
+++ include/element/modules/module.h
@@ -10,4 +10,11 @@ ELM_FUNCTION(module_exists, int, const char* name)
 ELM_FUNCTION(register_module, void, const element_module_t* module)
 ELM_FUNCTION(unregister_module, void, const char* name)
 
+typedef struct {
+    ELM_FUNCTION_IMPL(get_module);
+    ELM_FUNCTION_IMPL(module_exists);
+    ELM_FUNCTION_IMPL(register_module);
+    ELM_FUNCTION_IMPL(unregister_module);
+} element_module_module_t;
+
 ELM_END_C
