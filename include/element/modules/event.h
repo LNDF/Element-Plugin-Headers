59f5af31206120cbfca73fa00f7475bca8da4d0e
Lander Gallastegi <landergallastegi@gmail.com>
2024-04-18 11:39:17 +0200

    Fix module functions

--- include/element/modules/event.h
+++ include/element/modules/event.h
@@ -6,7 +6,7 @@
 ELM_BEGIN_C
 
 ELM_FUNCTION(register_event, element_event_t, const char*, void*)
-ELM_FUNCTION(unregister_event, void, element_event_t)
+ELM_FUNCTION(unregister_event, void, const char*, element_event_t)
 
 typedef struct {
     ELM_FUNCTION_IMPL(register_event);
